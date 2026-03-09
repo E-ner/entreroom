7889e48fa8ca6e6936926f0330703ece9a40a5ca
e-ner <nsengiyumva603@gmail.com>
2026-03-09 14:49:45 +0200

    feat: chat, notification configurations

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth/web-extension";
 import { getFirestore } from "firebase/firestore/lite";
+import { getMessaging } from "firebase/messaging/sw";
 const firebaseConfig = {
   apiKey: import.meta.env.FIREBASE_API_KEY,
   authDomain: import.meta.env.FIREBASE_AUTHDOMAIN,
@@ -11,3 +13,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const messaging = getMessaging(app);
