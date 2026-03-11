84ac21f1121fe957e05bdd0def361076d2d2a2e9
e-ner <nsengiyumva603@gmail.com>
2026-03-11 11:19:02 +0200

    refacto: accept changes from main

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,5 @@
 import { useEffect } from "react";
-import { app, db } from "./firebase";
-
-import { collection, addDoc } from "firebase/firestore";
 export default function App() {
-  const collections = collection(db, "work");
-
-  addDoc(collections, {
-    work: "Neric",
-  })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((e) => {
-      console.log(e);
-    });
   useEffect(() => {
     document.title = "Hellow";
   });
