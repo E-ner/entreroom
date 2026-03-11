edc1080f9e7f9df393444f6a77eb287dc5e7e1ce
e-ner <nsengiyumva603@gmail.com>
2026-03-11 10:16:37 +0200

    fix: change .env configurations in vite

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth/web-extension";
-import { getFirestore } from "firebase/firestore/lite";
+import { getFirestore } from "firebase/firestore";
 import { getMessaging } from "firebase/messaging/sw";
+
 const firebaseConfig = {
   apiKey: import.meta.env.FIREBASE_API_KEY,
   authDomain: import.meta.env.FIREBASE_AUTHDOMAIN,
@@ -11,7 +12,7 @@ const firebaseConfig = {
   appId: import.meta.env.FIREBASE_APPID,
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const messaging = getMessaging(app);
